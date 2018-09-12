a7da34498f271e986545051e5b0a5392abb2284f
waralika <waralikaprasad@gmail.com>
2018-09-12 18:21:36 +0530

    Add files via upload

---
+++ waralika_techaspect.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+class student{
+    public:
+    char name[100];
+  int age;
+  char branch[100];
+  int year;
+  int sem;
+  int score;
+  void display()
+  { cout<<"Name:"<<name<<" ";
+    cout<<"Age:"<<age<<" ";
+    cout<<"Branch:"<<branch<<" ";
+    cout<<"Semester:"<<sem<<" ";
+    cout<<"Score:"<<score<<endl;
+  }
+    void insert()
+  { cout<<"Enter Name, age, branch, semester, score(%)";
+    cin>>name>>age>>branch>>sem>>score;
+  }
+  
+ 
+  
+};
+
+int main() {
+	// your code goes here
+	int t,n;
+	student s[100];
+	cout<<"Enter no of students";
+	
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+	  s[i].insert();
+	}
+	
+	cout<<"\nEnter score greater than equal to filter";
+	cin>>t;
+	return 0;
+	
+	for(int j=0;j<n;j++)
+	{   
+	    
+	    if((s[j].score)>=t)
+	    {
+	        s[j].display();
+	    }
+	}
+}
